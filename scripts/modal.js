4b1191e8a01074d00cdd4958c8dd40140f858e24
GabrielBarrachino <gabrielbarrachino@outlook.com>
2023-12-14 18:04:59 -0300

    modal open finalizado

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,24 +1,49 @@
 const openModalLogin = document.getElementById('jsBtnLogin');
-const closeModalLogin = document.getElementById('jsBtnClose');
+const closeModalLogin = document.getElementById('jsBtnCloseLogin');
 const closeModalOverlayLogin = document.getElementById('overlayModal');
 const openModalLoginMobile = document.getElementById('jsBtnLoginMobile');
 
 openModalLogin.addEventListener('click', () => {
     let html = document.documentElement;
-    html.classList.add('showModal')
+    html.classList.add('showModalLogin')
 })
 
 openModalLoginMobile.addEventListener('click', () => {
     let html = document.documentElement;
-    html.classList.add('showModal')
+    html.classList.add('showModalLogin')
 })
 
 closeModalLogin.addEventListener('click', () => {
     let html = document.documentElement;
-    html.classList.remove('showModal')
+    html.classList.remove('showModalLogin')
 })
 
 closeModalOverlayLogin.addEventListener('click', () => {
     let html = document.documentElement;
-    html.classList.remove('showModal')
+    html.classList.remove('showModalLogin')
+})
+
+const openModalAccount = document.getElementById('jsBtnOpen')
+const closeModalAccount = document.getElementById('jsBtnCloseOpen')
+const closeModalOverlayAccount = document.getElementById('jsCloseOverlayOpen')
+const openModalAccountMobile = document.getElementById('jsBtnAccountMobile');
+
+openModalAccount.addEventListener('click', () => {
+    let html = document.documentElement;
+    html.classList.add('showModalOpen')
+})
+
+openModalAccountMobile.addEventListener('click', () => {
+    let html = document.documentElement;
+    html.classList.add('showModalOpen')
+})
+
+closeModalAccount.addEventListener('click', () => {
+    let html = document.documentElement;
+    html.classList.remove('showModalOpen')
+})
+
+closeModalOverlayAccount.addEventListener('click', () => {
+    let html = document.documentElement;
+    html.classList.remove('showModalOpen')
 })
